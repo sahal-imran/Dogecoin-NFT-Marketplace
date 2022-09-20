ccc5ba517f0ec9872386b371af407305c582559e
sahal imran <sahalimran786@gmail.com>
2022-09-20 10:07:04 +0500

    top bar added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/SideNavigationBar/SideNavigationBar";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
+import TopNavBar from "./components/TopNavBar/TopNavBar";
 
 function App() {
   return (
     <BrowserRouter>
+    <TopNavBar />
       <Sidebar>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/components/TopNavBar/TopNavBar.jsx
+++ src/components/TopNavBar/TopNavBar.jsx
@@ -11,295 +11,127 @@ import Badge from "@mui/material/Badge";
 import Hamburger from "hamburger-react";
 import Drawer from "react-modern-drawer";
 
-//import styles 👇
-import "react-modern-drawer/dist/index.css";
-
-function SideNavigationBar({ children }) {
+function TopNavBar() {
   const [isOpen, setOpen] = useState(false);
   const toggleDrawer = () => {
     setOpen((prevState) => !prevState);
   };
-
   return (
     <>
-      <Box sx={{ width: "100%", background: "#1E1E1E" }}>
-        <Grid container spacing={0}>
-          {/* Navigation bar side */}
-          <Grid
-            item
-            xs={2.3}
+      {/* Top bar */}
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: "80px",
+          px: 3,
+          background: "#1E1E1E",
+        }}
+      >
+        {/* Search and logo bar */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {/* Logo */}
+          <Box sx={{ width: "242px", height: "53px",mr:10 }}>
+            <img
+              src="/assets/logo.svg"
+              alt="logo"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+              }}
+            />
+          </Box>
+
+          {/* Seacrch */}
+          <Box
             sx={{
-              display: {
-                md: "block",
-                xs: "none",
-                borderRight: "0.5px solid #000000",
-              },
+              width: "400px",
+              height: "59px",
+              borderRadius: "10px",
+              background: "#242424",
+              overflow: "hidden",
+              display: { md: "block", xs: "none" },
             }}
           >
-            <Box
-              sx={{
+            <input
+              type="text"
+              style={{
                 width: "100%",
-                height: "calc(100vh - 80px)",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                flexDirection: "column",
-                px: { lg: 4, xs: 1 },
-                py: 3,
+                height: "100%",
+                background: "transparent",
+                fontFamily: "Syne",
+                color: "#B4B4B4",
+                fontSize: "18px",
+                lineHeight: "23.44px",
+                fontWeight: 400,
+                fontStyle: "normal",
+                outline: "none",
+                boxShadow: "unset",
+                border: "none",
+                padding: "0px 16px",
               }}
-            >
-              {/* Top sections */}
-              <Box
-                sx={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  flexDirection: "column",
-                }}
-              >
-                <Button
-                  variant="outlined"
-                  sx={{
-                    p: 0,
-                    width: "100%",
-                    height: "60px",
-                    borderRadius: "5px",
-                    borderColor: "#BA9F33",
-                    fontFamily: "Syne",
-                    color: "white",
-                    fontSize: "20px",
-                    lineHeight: "24px",
-                    fontWeight: 600,
-                    fontStyle: "normal",
-                    textTransform: "capitalize",
-                    "&:hover": {
-                      borderColor: "#BA9F33",
-                    },
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    src="/assets/marketplace.svg"
-                    alt="logo"
-                    style={{
-                      width: "30px",
-                      height: "30px",
-                      objectFit: "contain",
-                      marginRight: "16px",
-                    }}
-                  />
-                  Marketplace
-                </Button>
+              placeholder="Search collection or NFT"
+            />
+          </Box>
+        </Box>
+        {/* Profile and Button icons */}
+        <Box
+          sx={{
+            display: { md: "flex", xs: "none" },
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <IconButton sx={{ "&:hover": { background: "#242424" }, mr: 1 }}>
+            <Badge color="secondary" variant="dot">
+              <NotificationsNoneIcon
+                sx={{ color: "white", fontSize: "30px" }}
+              />
+            </Badge>
+          </IconButton>
+          <IconButton sx={{ "&:hover": { background: "#242424" }, mr: 1 }}>
+            <AccountCircleIcon sx={{ color: "white", fontSize: "30px" }} />
+          </IconButton>
 
-                {/* Nav Items */}
-                <Box
-                  sx={{
-                    mt: 2,
-                    pl: 3,
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "start",
-                    flexDirection: "column",
-                  }}
-                >
-                  <NavLink
-                    to={""}
-                    style={{
-                      textDecoration: "none",
-                      fontFamily: "Syne",
-                      color: "#858585",
-                      fontSize: "20px",
-                      lineHeight: "24px",
-                      fontWeight: 600,
-                      fontStyle: "normal",
-                      textTransform: "capitalize",
-                    }}
-                  >
-                    Popular Collection
-                  </NavLink>
-                  <NavLink
-                    to={""}
-                    style={{
-                      marginTop: "12px",
-                      textDecoration: "none",
-                      fontFamily: "Syne",
-                      color: "#858585",
-                      fontSize: "20px",
-                      lineHeight: "24px",
-                      fontWeight: 600,
-                      fontStyle: "normal",
-                      textTransform: "capitalize",
-                    }}
-                  >
-                    Newly Listed
-                  </NavLink>
-                  <NavLink
-                    to={""}
-                    style={{
-                      marginTop: "12px",
-                      textDecoration: "none",
-                      fontFamily: "Syne",
-                      color: "#858585",
-                      fontSize: "20px",
-                      lineHeight: "24px",
-                      fontWeight: 600,
-                      fontStyle: "normal",
-                      textTransform: "capitalize",
-                    }}
-                  >
-                    Aution
-                  </NavLink>
+          {/* Connect Wallet Button */}
+          <Button
+            variant="contained"
+            sx={{
+              p: 0,
+              width: "283px",
+              height: "56px",
+              borderRadius: "5px",
+              fontFamily: "Syne",
+              color: "white",
+              fontSize: "20px",
+              lineHeight: "24px",
+              fontWeight: 600,
+              fontStyle: "normal",
+              textTransform: "unset",
+              background: "linear-gradient(90deg, #BA9F33 0%, #E1CE83 100%)",
+            }}
+          >
+            Connect Wallet
+          </Button>
+        </Box>
 
-                  {/* Nav Items */}
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "start",
-                      flexDirection: "column",
-                    }}
-                  >
-                    {NavItems.map((item, index) => {
-                      return (
-                        <NavLink
-                          key={index}
-                          to={item.URL}
-                          style={{
-                            marginTop: "32px",
-                            textDecoration: "none",
-                            fontFamily: "Syne",
-                            color: "white",
-                            fontSize: "20px",
-                            lineHeight: "24px",
-                            fontWeight: 600,
-                            fontStyle: "normal",
-                            textTransform: "capitalize",
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                          }}
-                        >
-                          <img
-                            src={item.ico}
-                            alt="logo"
-                            style={{
-                              width: "32px",
-                              height: "32px",
-                              objectFit: "contain",
-                              marginRight: "12px",
-                            }}
-                          />
-                          {item.Name}
-                        </NavLink>
-                      );
-                    })}
-                  </Box>
-                </Box>
-              </Box>
+        {/* +++++++++++++++++++ */}
+        {/* For phone */}
+        {/* +++++++++++++++++++ */}
 
-              {/* Bottom sections */}
-              <Box
-                sx={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  flexDirection: "column",
-                }}
-              >
-                <Button
-                  variant="contained"
-                  sx={{
-                    p: 0,
-                    width: "100%",
-                    height: "56px",
-                    borderRadius: "5px",
-                    fontFamily: "Syne",
-                    color: "white",
-                    fontSize: "20px",
-                    lineHeight: "24px",
-                    fontWeight: 600,
-                    fontStyle: "normal",
-                    textTransform: "unset",
-                    background:
-                      "linear-gradient(90deg, #BA9F33 0%, #E1CE83 100%)",
-                  }}
-                >
-                  Create NFTs
-                </Button>
-
-                {/* Social icons */}
-                <Box
-                  sx={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    mt: 2,
-                  }}
-                >
-                  {Socail_Links.map((item, index) => {
-                    return (
-                      <Link
-                        key={index}
-                        href={item.ref}
-                        target={"_blank"}
-                        sx={
-                          index === 3
-                            ? {
-                                width: "24px",
-                                height: "24px",
-                                textDecoration: "none",
-                              }
-                            : {
-                                width: "24px",
-                                height: "24px",
-                                textDecoration: "none",
-                                mr: 2,
-                              }
-                        }
-                      >
-                        <img
-                          src={item.icoURL}
-                          alt="logo"
-                          style={{
-                            width: "100%",
-                            height: "100%",
-                            objectFit: "contain",
-                          }}
-                        />
-                      </Link>
-                    );
-                  })}
-                </Box>
-              </Box>
-            </Box>
-          </Grid>
-
-          {/* Content Side */}
-          <Grid item xs={12} md={9.7}>
-            <Box
-              sx={{
-                width: "100%",
-                minHeight: "100vh",
-                display: "flex",
-                justifyContent: "start",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <Box sx={{ p: 3, width: "100%", overflow: "hidden" }}>
-                <main>{children}</main>
-              </Box>
-            </Box>
-          </Grid>
-        </Grid>
+        <Box sx={{ display: { md: "none", xs: "block" } }}>
+          <Hamburger color="white" toggled={isOpen} toggle={toggleDrawer} />
+        </Box>
       </Box>
-
       {/* Drawer */}
       <Drawer
         open={isOpen}
@@ -590,7 +422,7 @@ function SideNavigationBar({ children }) {
   );
 }
 
-export default SideNavigationBar;
+export default TopNavBar;
 
 const Socail_Links = [
   {
